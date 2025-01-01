522715e79dd674128b1024229bf64bd4d1258900
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-01 16:26:29 +0530

    Changes for the file

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,33 @@ import Player from "./components/Player";
 import Gammeboard from './components/Gameboard';
 import Log from './components/Log';
 
+function derivedActivePlayer(gameTurns){
+  let currentPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
+
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
+ // const [activePlayer, setActivePlayer] = useState('X');
   const [gameTurns, setGameTurns] = useState([]);
 
+  const activePlayer = derivedActivePlayer(gameTurns);
+
   const handleSelectSquare = (rowIndex, colIndex) => {
-    setActivePlayer((currentActive) => {
-      return currentActive === 'X' ? 'O' : 'X';
-    });
+    // setActivePlayer((currentActive) => {
+    //   return currentActive === 'X' ? 'O' : 'X';
+    // });
+  
 
     setGameTurns(prevTurns => {
-      let currentPlayer = 'X';
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+      // let currentPlayer = 'X';
+      // if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+      //   currentPlayer = 'O';
+      // }
+      const currentPlayer = derivedActivePlayer(prevTurns);
+      
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
         ...prevTurns,
